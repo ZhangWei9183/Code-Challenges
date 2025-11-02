07aa16ec6d904df49e07a555b2a21f0b57a147f1
ZhangWei9183 <ZhangWei9183@outlook.com>
2025-11-02 15:36:44 +0000

    Implement maxEdgesToAdd method in Solution class

---
+++ GeeksforGeeks/Max DAG Edges.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    
+    public
+    int
+    maxEdgesToAdd
+    (
+        int V
+        , int[][] edges
+    )
+    {
+        return ( V * ( V - 1 ) / 2 ) - edges.length;
+    }
+    
+}
